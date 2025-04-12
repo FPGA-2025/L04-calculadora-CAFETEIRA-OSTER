03b74fa95f0c6748054b96bb4710bf0dfcdfc961
Yuri Bernardo <yuribernardosie@gmail.com>
2025-04-12 11:59:00 -0300

    correção de código

--- calculadora.v
+++ calculadora.v
@@ -4,13 +4,10 @@ module calculadora(
   input   [2:0] codigo,
   output  [7:0] saida
 );
-
-  always @(*) begin
     assign saida = (codigo == 3'b000) ? 8'b00000000 : // zerar
                    (codigo == 3'b001) ? entrada_A :   // mostrar_A
                    (codigo == 3'b010) ? entrada_B :   // mostrar_B
                    (codigo == 3'b011) ? entrada_A + entrada_B : // somar
                    (codigo == 3'b100) ? entrada_A - entrada_B : // subtrair
                    8'b00000000; // operação inválida (zerar)
-  end
 endmodule
